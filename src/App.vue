3d41c2de3f5ff7e64083ffbe9c3fb6a077ef1917
Chill Fish <chill4fish@gmail.com>
2023-06-28 18:30:54 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<!-- eslint-disable no-console -->
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <button @click="count++">
+    count is: {{ count }}
+  </button>
+</template>
+
+<style scoped>
+</style>
