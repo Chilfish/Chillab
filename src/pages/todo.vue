31de7f28356513eaf08423ffe32ddb5125785798
Chill Fish <chill4fish@gmail.com>
2023-07-27 18:31:26 +0800

    feat(store): use pinia to store todo

--- src/pages/todo.vue
+++ src/pages/todo.vue
@@ -1,90 +1,68 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { createReusableTemplate, useArrayFilter } from '@vueuse/core'
+import { createReusableTemplate } from '@vueuse/core'
 import { useSortable } from '@vueuse/integrations/useSortable'
+
 import type { todoItem } from '~/types'
+import { useTodoStore } from '~/store/useTodo'
 
-const newTodo = ref('')
-const todoArr = ref<todoItem[]>([
-  { id: '1', content: 'Buy a car', done: false },
-  { id: '2', content: 'Buy a house', done: false },
-  { id: '3', content: 'Buy a boat', done: false },
-])
-
-function addTodo() {
-  const content = newTodo.value.trim()
-  if (!content || content === '')
-    return
-
-  const newTodoItem = {
-    id: Date.now().toString(),
-    content,
-    done: false,
-  }
-
-  todoArr.value.push(newTodoItem)
-  newTodo.value = ''
-}
-
-const completedTodos = useArrayFilter(todoArr, todo => todo.done)
-const uncompletedTodos = useArrayFilter(todoArr, todo => !todo.done)
+const useTodo = useTodoStore()
 
 const {
   define: DTodoList,
   reuse: TodoList,
 } = createReusableTemplate<{ list: todoItem[] }>()
 
-useSortable('#completed', completedTodos, {
+useSortable('#completed', useTodo.completedTodos, {
   animation: 150,
 })
 
-useSortable('#uncompleted', uncompletedTodos, {
+useSortable('#uncompleted', useTodo.uncompletedTodos, {
   animation: 150,
 })
 </script>
 
 <template>
   <DTodoList v-slot="{ list }">
-    <ul :id="list === completedTodos ? 'completed' : 'uncompleted'">
+    <ul :id="list === useTodo.completedTodos ? 'completed' : 'uncompleted'">
       <li v-for="todo in list" :key="todo.id">
         <TodoItem
           :item="todo"
-          @toggle="todo.done = !todo.done"
-          @delete="todoArr.splice(todoArr.indexOf(todo), 1)"
+          @toggle="useTodo.toggleTodoItem(todo.id)"
+          @delete="useTodo.deleteTodoItem(todo.id)"
         />
       </li>
     </ul>
   </DTodoList>
 
-  <main class="flex flex-col w-full">
+  <main class="w-full flex flex-col">
     <h2> Chill Todo List </h2>
     <label class="my-6 w-full flex">
       <input
-        v-model="newTodo"
+        v-model="useTodo.newTodo"
         type="text"
         placeholder="Add a new todo"
         class="mr-3 h-14 w-full rounded-3 bg-dark-50 p-4"
-        @keyup.enter="addTodo"
+        @keyup.enter="useTodo.addTodo"
       >
       <button
         class="h-14 w-18 rounded-3 btn"
-        @click="addTodo"
+        @click="useTodo.addTodo"
       >
         Add
       </button>
     </label>
 
-    <TodoList :list="uncompletedTodos" />
+    <TodoList :list="useTodo.uncompletedTodos" />
 
     <details
-      v-show="completedTodos.length"
+      v-show="useTodo.completedTodos.length"
       open
     >
       <summary class="mb-2 mt-4 cursor-pointer">
-        Completed {{ completedTodos.length }}
+        Completed {{ useTodo.completedTodos.length }}
       </summary>
 
-      <TodoList :list="completedTodos" />
+      <TodoList :list="useTodo.completedTodos" />
     </details>
   </main>
 </template>
---
+++ src/store/useTodo.ts
@@ -0,0 +1,57 @@
+import { useArrayFilter } from '@vueuse/core'
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import type { todoItem } from '~/types'
+
+export const useTodoStore = defineStore('todo', () => {
+  const newTodo = ref('')
+  const todoArr = ref<todoItem[]>([
+    { id: '1', content: 'Buy a car', done: false },
+    { id: '2', content: 'Buy a house', done: false },
+    { id: '3', content: 'Buy a boat', done: false },
+  ])
+
+  function addTodo() {
+    const content = newTodo.value.trim()
+    if (!content || content === '')
+      return
+
+    const newTodoItem = {
+      id: Date.now().toString(),
+      content,
+      done: false,
+    }
+
+    todoArr.value.push(newTodoItem)
+    newTodo.value = ''
+  }
+
+  function deleteTodoItem(id: string) {
+    todoArr.value = todoArr.value.filter(todo => todo.id !== id)
+  }
+
+  function toggleTodoItem(id: string) {
+    todoArr.value = todoArr.value.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          done: !todo.done,
+        }
+      }
+      return todo
+    })
+  }
+
+  const completedTodos = useArrayFilter(todoArr, todo => todo.done)
+  const uncompletedTodos = useArrayFilter(todoArr, todo => !todo.done)
+
+  return {
+    newTodo,
+    todoArr,
+    completedTodos,
+    uncompletedTodos,
+    addTodo,
+    deleteTodoItem,
+    toggleTodoItem,
+  }
+})
